38a415af4f0fb9703d76e2fa0ef75d4be8486055
Shreya Paranjape <shreyaparanjape3@gmail.com>
2024-02-21 20:21:10 +0530

    Initial Commit

---
+++ PracticalNo4.java
@@ -0,0 +1,21 @@
+public class PracticalNo4 
+{
+    public static void printName(String name) 
+    {
+        
+        name = name.trim();
+       
+        String[] names = name.split("\\s+");
+       
+        String firstName = names[0];
+        String lastName = names[names.length - 1];
+       
+        System.out.println(lastName + ", " + firstName);
+    }
+   
+    public static void main(String[] args) 
+    {
+        printName("Bill Joy");
+    }
+}
+
